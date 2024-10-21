a509ed0141f61a7a791d0d7a49920e6b111c7c5a
Maajid Hussain <ksmaajidhussain@gmail.com>
2024-10-21 21:32:51 +0530

    Update common elements(tuple).py

--- common elements(tuple).py
+++ common elements(tuple).py
@@ -1,7 +1,9 @@
-def common_elements(tuple1, tuple2):
-    common=tuple(set(tuple1) & set(tuple2))
-    return common
-tuple1 = (1, 2, 3, 4, 5)
-tuple2 = (4, 5, 6, 7, 8)
-output_tuple = common_elements(tuple1, tuple2)
-print(output_tuple)
+def commonElements(List1,List2):
+    #Using set() to Avoid Occuring Duplicates..
+    common_elements=list(set(List1) & set(List2))
+    
+    return common_elements
+List1= [1, 2, 3, 4, 4, 5, 5]
+List2= [4, 5, 6, 7, 4, 5, 8]
+common=commonElements(List1,List2)
+print("Common Elements in given two lists are:",common)
