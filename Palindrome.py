d5aa1d3854b3827126d9c34351abfce270b2872a
Maajid Hussain <ksmaajidhussain@gmail.com>
2024-10-21 17:49:43 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,10 +1,10 @@
-def func(Input):
+def Palindrome(Input):
     Input_str=str(Input)
     rev_str=Input_str[::-1]
     return Input_str==rev_str
     
 Input=input("Enter any String / Number: ")
-if func(Input):
+if Palindrome(Input):
     print(True)
 else:
     print(False)
